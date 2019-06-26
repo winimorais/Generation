9436fe4c8d06f629bdb2f6d10e29231ce64d8b76
winimorais <winimorais@gmail.com>
2019-06-26 17:58:56 -0300

    Adicionado Aula 2 de Spring_Boot

---
+++ Exercicios/Spring_Boot_Java/Aula_2/gfood/gfood/gfood/src/main/java/org/generation/brazil/gfood/cliente/ClienteController.java
@@ -0,0 +1,28 @@
+package org.generation.brazil.gfood.cliente;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+public class ClienteController {
+
+    @Autowired
+    private ClienteRepository repository;
+
+    @GetMapping("/clientes")
+    public List<Cliente> findAll() {
+
+        return repository.findAll();
+    }
+
+    @ResponseStatus(HttpStatus.CREATED)
+    @PostMapping("/clientes")
+    public Cliente save(@RequestBody Cliente cliente) {
+        return repository.save(cliente);
+    }
+
+
+}
